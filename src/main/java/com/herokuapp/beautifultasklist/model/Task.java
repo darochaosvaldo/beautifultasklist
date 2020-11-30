abc66b505f4d49dd105acea77ffdbbbc1924875c
Osvaldo da Rocha <osvaldodarocha1992@yahoo.com.br>
2020-11-29 22:44:14 -0300

    wip: entity manager test

--- src/main/java/com/herokuapp/beautifultasklist/model/Task.java
+++ src/main/java/com/herokuapp/beautifultasklist/model/Task.java
@@ -2,99 +2,97 @@ package com.herokuapp.beautifultasklist.model;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Column;
+import javax.persistence.Id;
 
-@Entity
+@Entity(name = "tb_task")
 public class Task {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	@Column(name="id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
 	private Integer id;
 
-	@Column(name="ds_title")
+	@Column(name = "ds_title")
 	private String title;
-	@Column(name="ds_description")
+	@Column(name = "ds_description")
 	private String description;
-	@Column(name="st_status")
+	@Column(name = "st_status")
 	private Boolean status;
-	@Column(name="dt_creation")
+	@Column(name = "dt_creation")
 	private Date creation;
-	@Column(name="dt_last_edition")
+	@Column(name = "dt_last_edition")
 	private Date lastEdition;
-	@Column(name="dt_removal")
+	@Column(name = "dt_removal")
 	private Date removal;
-	@Column(name="dt_conclusion")
+	@Column(name = "dt_conclusion")
 	private Date conclusion;
 
 	public Integer getId() {
-    	return id;
-  	}
+		return this.id;
+	}
 
-  	public void setId(Integer id) {
-    	this.id = id;
-  	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-  	public String getTitle() {
-    	return title;
-  	}
+	public String getTitle() {
+		return this.title;
+	}
 
-  	public void setTitle(String title) {
-    	this.title = title;
-  	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-  	public String getDescription() {
-    	return description;
-  	}
+	public String getDescription() {
+		return this.description;
+	}
 
-  	public void setDescription(String description) {
-    	this.description = description;
-  	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-  	public Boolean getStatus() {
-    	return status;
-  	}
+	public Boolean getStatus() {
+		return this.status;
+	}
 
-  	public void setStatus(Boolean status) {
-    	this.status = status;
-  	}
+	public void setStatus(Boolean status) {
+		this.status = status;
+	}
 
+	public Date getCreation() {
+		return this.creation;
+	}
 
-  	public Date getCreation() {
-    	return creation;
-  	}
+	public void setCreation(Date creation) {
+		this.creation = creation;
+	}
 
-  	public void setCreation(Date creation) {
-    	this.creation = creation;
-  	}
+	public Date getLastEdition() {
+		return this.lastEdition;
+	}
 
-  	public Date getLastEdition() {
-    	return lastEdition;
-  	}
+	public void setLastEdition(Date lastEdition) {
+		this.lastEdition = lastEdition;
+	}
 
-  	public void setLastEdition(Date lastEdition) {
-    	this.lastEdition = lastEdition;
-  	}
+	public Date getRemoval() {
+		return this.removal;
+	}
 
-  	public Date getRemoval() {
-    	return removal;
-  	}
+	public void setRemoval(Date removal) {
+		this.removal = removal;
+	}
 
-  	public void setRemoval(Date removal) {
-    	this.removal = removal;
-  	}
+	public Date getConclusion() {
+		return this.conclusion;
+	}
 
-  	public Date getConclusion() {
-    	return conclusion;
-  	}
+	public void setConclusion(Date conclusion) {
+		this.conclusion = conclusion;
+	}
 
-  	public void setConclusion(Date conclusion) {
-    	this.conclusion = conclusion;
-  	}
-
-  
 }
